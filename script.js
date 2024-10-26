f35d7927e1109a9c477e9e1d1c09b81b74a93ac6
agburse-mtb <151734019+agburse-mtb@users.noreply.github.com>
2024-10-26 18:31:06 +0200

    2024 Update script.js aktualisieren

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
   const counterText = document.getElementById("counterText");
 
-  // Set the date for October 1st, 2023
-  const octoberFirst = new Date("2023-10-01");
+  // Set the date for October 1st, 2024
+  const octoberFirst = new Date("2024-10-01");
 
   function updateCounter() {
     // Get the current date and time
